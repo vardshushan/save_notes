f2bff1c62bf9b58addcb2ce88b90acae21efed1d
Shushan <shushanvardanyan03@gmail.com>
2024-03-04 19:15:58 +0400

    swagger for getting  links

--- app/Http/Controllers/LinksController.php
+++ app/Http/Controllers/LinksController.php
@@ -5,14 +5,36 @@ namespace App\Http\Controllers;
 use App\Http\Requests\Links\CreateLinkRequest;
 use App\Services\Links\LinksService;
 use Illuminate\Http\JsonResponse;
+use OpenApi\Attributes as OA;
 
 class LinksController extends Controller
 {
     public function __construct( private readonly LinksService $linksService)
     {}
-    /**
-     * Display a listing of the resource.
-     */
+
+    #[OA\Get(
+        path: '/api/links',
+        operationId: 'getSavedLinks',
+        summary: 'get Saved links',
+        requestBody: new OA\RequestBody(
+            description: 'get Saved links',
+            content: new OA\JsonContent(
+                required: [],
+                properties: []
+            )
+        ),
+        tags: ['Saved Links'],
+        responses: [
+            new OA\Response(
+                ref: '#/components/responses/SuccessResponse',
+                response: 200
+            ),
+            new OA\Response(
+                ref: '#/components/responses/UnAuthorizedResource',
+                response: 401
+            ),
+        ]
+    )]
     public function index(): JsonResponse
     {
         return $this->linksService->getLinks();
